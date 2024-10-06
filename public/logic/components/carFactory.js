af327a82aa5b4d958f805f1668b328ca470a6244
Ilias Elbadaoui <iliassovich@outlook.com>
2024-10-06 14:01:25 +0100

    Implemented car factory pagination

--- public/logic/components/carFactory.js
+++ public/logic/components/carFactory.js
@@ -2,49 +2,77 @@ import * as builder from '../vendors/builder.js';
 import Car from './car.js';
 import waitScreen from './waitScreen.js';
 
-export default class CarFactory extends builder.Component
-{
-	constructor()
-	{
-		super()
-		this.create()
-	}
+export default class CarFactory extends builder.Component {
+    constructor() {
+        super();
+        this.currentPage = 1;
+        this.pageSize = 1; // You can adjust this value as needed
+        this.totalPages = 1;
+        this.create();
+    }
 
+    #factory(data) {
+        const cars = [];
+        for (const car of data) {
+            cars.push((new Car(car, this)).getHTML());
+        }
+        return cars;
+    }
 
-	#factory(data)
-	{
-		const cars = []
-		for (const car of data)
-		{
-			cars.push((new Car(car, this)).getHTML())
-		}
-		return cars
-	}
+    #createPaginationControls() {
+        const paginationContainer = builder.block(null, 'pagination-controls', []);
+        
+        const prevButton = builder.button(null ,'pagination_button', null, '<i class="ri-arrow-left-circle-fill"></i>');
+		prevButton.onclick = () => {this.component.className = 'carFactoryInitial'; this.changePage(this.currentPage - 1)}
+        prevButton.disabled = this.currentPage === 1;
+        
+        const nextButton = builder.button(null, 'pagination_button', null, '<i class="ri-arrow-right-circle-fill"></i>');
+		nextButton.onclick = () => {this.component.className = 'carFactoryInitial'; this.changePage(this.currentPage + 1)}
+        nextButton.disabled = this.currentPage === this.totalPages;
+        
+        const pageInfo = builder.label('page-info', `Page ${this.currentPage} of ${this.totalPages}`);
+        
+        paginationContainer.append(prevButton, pageInfo, nextButton);
+        return paginationContainer;
+    }
 
-	create()
-	{
-		let wait_screen = new waitScreen(),
-			empty_message = builder.label('empty_message', 'Aucune voiture trouvée dans cette agence'),
-			fd = new FormData();
-		
-		const user = JSON.parse(builder.prefs.get('user'))
-		fd.append('agency', user.id)
+    changePage(newPage) {
+        if (newPage >= 1 && newPage <= this.totalPages) {
+            this.currentPage = newPage;
+            this.loadCars();
+        }
+    }
 
-		this.component = builder.block(null, 'carFactoryInitial', [wait_screen.getHTML()])
+    loadCars() {
+        let wait_screen = new waitScreen();
+        let empty_message = builder.label('empty_message', 'Aucune voiture trouvée dans cette agence');
+        let fd = new FormData();
+        
+        const user = JSON.parse(builder.prefs.get('user'));
+        fd.append('agency', user.id);
+        fd.append('page', this.currentPage);
+        fd.append('pageSize', this.pageSize);
 
-		builder.brdige("/agency/getAllCars", "GET", fd, (res)=>{
-			res = JSON.parse(res)
-			if  (res.hasOwnProperty('code') && (res.code == 3 || res.code == -1))
-			{
-				this.component.innerHTML = ''
-				this.component.appendChild(empty_message)
-			}
-			else
-			{
-				this.component.innerHTML = ''
-				this.component.className = 'carFactoryGrid'
-				this.component.append(...this.#factory(res))
-			}
-		}, ()=>{})
-	}
+        this.component.innerHTML = '';
+        this.component.appendChild(wait_screen.getHTML());
+
+        builder.brdige("/agency/getAllCars", "GET", fd, (res) => {
+            res = JSON.parse(res);
+            this.component.innerHTML = '';
+
+            if (res.hasOwnProperty('code') && (res.code == 3 || res.code == -1)) {
+                this.component.appendChild(empty_message);
+            } else {
+				this.component.className = 'carFactoryFinal'
+                this.totalPages = res.pagination.totalPages;
+                this.component.append(builder.block(null, "carFactoryGrid", [...this.#factory(res.cars)]));
+                this.component.appendChild(this.#createPaginationControls());
+            }
+        }, () => {});
+    }
+
+    create() {
+        this.component = builder.block(null, 'carFactoryInitial');
+        this.loadCars();
+    }
 }
